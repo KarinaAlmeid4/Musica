e29b6d1024ed64080f89b03a5bcdc624ee1e0e05
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 20:09:34 -0300

    Tarefa de musica finalizada

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write("Angels speak - Justin Bieber");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"Your voice is music to my ears, I'm wide awake
+Stuck on the way your frequencies resonate
+You say that no matter what, you're watching over me
+I knew when I first read your lips
+Only angels speak like this");
+
+Console.ResetColor();
+
+Console.WriteLine(@"You encourage everyone without trying
+All of your boundaries keep me in line
+God is within us, what you're always saying
+Your energy is enough to suffice
+
+Your intentions are obvious
+Your affection's miraculous
+Every promise you make, you keep
+Never met nobody else like this
+Only angels speak like this");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Your voice is music to my ears, I'm wide awake
+Stuck on the way your frequencies resonate
+You say that no matter what, you're watching over me
+I knew when I first read your lips
+Only angels speak like this");
+
+Console.ResetColor();
+
+Console.WriteLine(@"La-di-da, da-da-di-da
+La-di-da, la-di-da
+La-di-da, da-da-di-da
+Di-da, la-da
+La-di-da, da-da-di-da
+Di-da, da-da
+Only angels speak like this
+
+The cadence you speak, it keeps us both in sync
+The ocean we lay in makes it hard to think
+Our spirits combine and it's a basic instinct
+Your love is free-spoken, not written in ink
+(Oh, oh)
+
+Your intentions are obvious (obvious)
+Your affection's miraculous (I cherish your touch)
+Every promise you make, you keep (oh, oh)
+Never met nobody else like this
+Only angels speak like this");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Your voice is music to my ears, I'm wide awake (I'm wide awake)
+Stuck on the way your frequencies resonate (how they resonate)
+You say that no matter what (no matter what)
+You're watching over me (you're watching over me)
+I knew when I first read your lips (your lips)
+Only angels speak like this (like this)");
+
+Console.ResetColor();
+
+Console.WriteLine(@"La-di-da, da-da-di-da (La)
+La-di-da (la), la-di-da (la, ah)
+La-di-da, da-da-di-da (la)
+Di-da (la), da-da (yea, yeah)
+La-di-da (la), da-da-di-da (la)
+Di-da, da-da (da-da)
+Only angels speak like this (woah, oh, ooh)");
